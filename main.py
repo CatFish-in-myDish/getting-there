9e82a42b08f5672cf66c9f20662db1087371fcd0
catfish-in-mydish <sanjay.pandian@tutamail.com>
2026-04-17 21:22:03 +0530

    added a new module and changed some

---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import parser
+
+# print(parser.fetch_vechicles("https://wiki.warthunder.com/collections/operator/country_japan"))
+parser.fetch_country_dict()
+print("HIII")
